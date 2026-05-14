85be26fe54f7e3ac2afd0b454dd14c1bc816b2a2
tinoabello <tino.abello.oller@gmail.com>
2026-05-14 20:28:56 +0200

    añado la biblio.java

---
+++ Biblioteca.java
@@ -0,0 +1,20 @@
+package Proyecto_ED;
+
+import java.util.ArrayList;
+import java.util.List;
+public class Biblioteca {
+private List<Llibre> llibres;
+public Biblioteca() {
+this.llibres = new ArrayList<>();
+}
+public void afegirLlibre(Llibre llibre) { llibres.add(llibre); }
+public Llibre buscarLlibre(String titol) {
+for (Llibre llibre : llibres) {
+if (llibre.getTitol().equalsIgnoreCase(titol)) {
+return llibre;
+}
+}
+return null;
+}
+public List<Llibre> getLlibres() { return llibres; }
+}
